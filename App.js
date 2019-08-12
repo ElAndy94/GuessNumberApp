d99ce1ffe064fb2bda05361faf5324a335566acd
Andrew <andrewpeliza@hotmail.com>
2019-08-12 20:31:58 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title='Guess a Number' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
