30e2d0411ec302894f119a18a64ae1f9103899f3
Andrew <andrewpeliza@hotmail.com>
2019-08-15 17:52:00 +0100

    Game now can be reset and shows you scores at the end...

--- App.js
+++ App.js
@@ -10,9 +10,13 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
-    setGuessRounds(0);
   };
 
   const gameOverHandler = numOfRounds => {
@@ -26,7 +30,13 @@ export default function App() {
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />;
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Game is over...</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title='NEW GAME' onPress={props.onRestart} />
     </View>
   );
 };
