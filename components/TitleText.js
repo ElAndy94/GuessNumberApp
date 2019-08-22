a9d4251eb4596ff3fff8ce55cd85d8e4915c1cb9
Andrew <andrewpeliza@hotmail.com>
2019-08-22 12:39:59 +0100

    Default styles instead of creating a component for each.

--- components/TitleText.js
+++ components/TitleText.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const TitleText = props => <Text style={styles.body}>{props.children}</Text>;
+const TitleText = props => (
+  <Text style={{ ...styles.body, ...props.style }}>{props.children}</Text>
+);
 
 const styles = StyleSheet.create({
   body: {
-    fontFamily: 'open-sans-bold'
+    fontFamily: 'open-sans-bold',
+    fontSize: 18
   }
 });
 
